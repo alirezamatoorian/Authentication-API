5d1ac58444b31075aa2cd0c2070931cc3a3ae806
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-09-06 18:56:40 +0330

    coding custom user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from .manager import UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
 
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField()
+    bio = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
