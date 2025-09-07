180ebbb36a71e6938623dfe182f1beddf2e3a00d
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-07 14:25:27 +0330

    perf(models): add index for faster lookup on email

--- account/models.py
+++ account/models.py
@@ -13,8 +13,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(max_length=11)
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
-    avatar = models.ImageField(blank=True,null=True)
-    bio = models.TextField(blank=True,null=True)
+    avatar = models.ImageField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -33,6 +33,9 @@ class Otp(models.Model):
     code = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
     is_used = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = models.Index(fields=['email'],)
 
     def is_expired(self):
         return timezone.now() > self.created_at + timedelta(minutes=3)
