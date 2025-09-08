8d5e401cc4f72c82bff03cff50050b0015653c2c
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-08 14:31:23 +0330

    feat(view) add send otp and verify otp

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import SendOtpSerializer
+
 
 # Create your views here.
+
+
+class SendOtpView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = SendOtpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        result = serializer.save()
+        return Response(result, status=status.HTTP_201_CREATED)
+
+
+class VerifyOtpView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = SendOtpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        result = serializer.save()
+        return Response(result, status=status.HTTP_201_CREATED)
