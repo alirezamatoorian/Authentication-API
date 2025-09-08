72e35aa50d7883b812f89d0073dab61e4515a447
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-08 15:29:33 +0330

    fixing some isue

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import SendOtpSerializer, VerifyOtpSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
