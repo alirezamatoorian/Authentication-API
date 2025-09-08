e27dab990a68e0da802ba6c8770bed8c02be134a
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-08 14:47:36 +0330

    feat(urls): add send oto and verify otp urls

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = "account"
 
 urlpatterns = [
+    path("send-otp/", views.SendOtpView.as_view(), name='send-otp'),
+    path("verify-otp", views.VerifyOtpView.as_view(), name='verify-otp')
 
 ]
