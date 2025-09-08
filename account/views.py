7b53dbce4a39753a8f3fcb44f77692c44f930a88
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-08 18:46:49 +0330

    feat: coding acces and refersh token

--- account/views.py
+++ account/views.py
@@ -21,5 +21,11 @@ class VerifyOtpView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = VerifyOtpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        result = serializer.save()
-        return Response(result, status=status.HTTP_201_CREATED)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+        return Response(tokens, status=status.HTTP_200_OK)
