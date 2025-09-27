3a197cd10a606d146e7d50927ca95ceb047b6c9d
Alireza <ali.r.matourianpoor@gmail.com>
2025-09-27 14:45:28 +0330

    change otp code task func name

--- account/tasks.py
+++ account/tasks.py
@@ -7,7 +7,7 @@ from .models import Otp
 
 
 @shared_task
-def cleanup_otps():
+def remove_expired_otp():
     expired = Otp.objects.filter(is_used=True) | Otp.objects.filter(created_at__lt=now() - timedelta(minutes=5))
     count = expired.count()
     expired.delete()
